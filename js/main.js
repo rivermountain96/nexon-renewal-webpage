eefa0a3f3903d4acc4fedc17a35fc9b6fc78df3f
rivermountain96 <dlrkdtks7@gmail.com>
2023-06-28 22:41:52 +0900

    생성: main2 javascript 생성

--- js/main.js
+++ js/main.js
@@ -1,110 +1,193 @@
 /* 이현정 구현 부분*/ 
 /* main carousel */
 
-let slideWrapper = document.querySelector(".mainslide-wrapper");
-let slideContainer = slideWrapper.querySelector(".mainslide-container");
-let slides = slideContainer.querySelectorAll(".mainslide");
-let slideCount = slides.length;
-let currentSlideIdx = 0;
-let pager = slideWrapper.querySelector(".pager");
-let timer;
-let pagerHTML = "";
-let slideWidth = document.body.offsetWidth;
-let prevBtn = slideWrapper.querySelector("#prev");
-let nextBtn = slideWrapper.querySelector("#next");
+// let slideWrapper = document.querySelector(".mainslide-wrapper");
+// let slideContainer = slideWrapper.querySelector(".mainslide-container");
+// let slides = slideContainer.querySelectorAll(".mainslide");
+// let slideCount = slides.length;
+// let currentSlideIdx = 0;
+// let pager = slideWrapper.querySelector(".pager");
+// let timer;
+// let pagerHTML = "";
+// let slideWidth = document.body.offsetWidth;
+// let prevBtn = slideWrapper.querySelector("#prev");
+// let nextBtn = slideWrapper.querySelector("#next");
 
-function setSlide() {
-  if (slideCount > 1) {
-    let containerWidth = slideWidth * slideCount;
-    slideContainer.style.width = `${containerWidth}px`;
+// function setSlide() {
+//   if (slideCount > 1) {
+//     let containerWidth = slideWidth * slideCount;
+//     slideContainer.style.width = `${containerWidth}px`;
 
-    slides.forEach((item, idx) => {
-      item.style.width = `${slideWidth}px`;
-      let url = item.getAttribute("data-url");
-      let title = item.getAttribute("data-title");
-      console.log(url)
-      console.log(title)
+//     slides.forEach((item, idx) => {
+//       item.style.width = `${slideWidth}px`;
+//       let url = item.getAttribute("data-url");
+//       let title = item.getAttribute("data-title");
+//       console.log(url)
+//       console.log(title)
 
-      pagerHTML += `<a href="">
-      <span>
-        <img
-          src=${url}
-        />
-      </span>
-      <span>${title}</span>
-    </a>
-    `;
-    });
-  }
-}
+//       pagerHTML += `<a href="">
+//       <span>
+//         <img
+//           src=${url}
+//         />
+//       </span>
+//       <span>${title}</span>
+//     </a>
+//     `;
+//     });
+//   }
+// }
 
-setSlide();
+// setSlide();
 
-pager.innerHTML = pagerHTML;
-let pagerBtn = pager.querySelectorAll("a");
+// pager.innerHTML = pagerHTML;
+// let pagerBtn = pager.querySelectorAll("a");
 
-function moveSlide(num) {
-  slideContainer.style.transform = `translateX(${-num * slideWidth}px)`;
-  currentSlideIdx = num;
-  console.log(currentSlideIdx);
+// function moveSlide(num) {
+//   slideContainer.style.transform = `translateX(${-num * slideWidth}px)`;
+//   currentSlideIdx = num;
+//   console.log(currentSlideIdx);
 
-  updateNav();
-}
+//   updateNav();
+// }
 
-updateNav();
+// updateNav();
 
-function updateNav() {
-  for (let sl of slides) {
-    sl.classList.remove("active");
-  }
-  slides[currentSlideIdx].classList.add("active");
+// function updateNav() {
+//   for (let sl of slides) {
+//     sl.classList.remove("active");
+//   }
+//   slides[currentSlideIdx].classList.add("active");
 
-  for (let pb of pagerBtn) {
-    pb.classList.remove("active");
-  }
-  pagerBtn[currentSlideIdx].classList.add("active");
-}
+//   for (let pb of pagerBtn) {
+//     pb.classList.remove("active");
+//   }
+//   pagerBtn[currentSlideIdx].classList.add("active");
+// }
 
-nextBtn.addEventListener("click", () => {
-  if (currentSlideIdx < slideCount - 1) {
-    moveSlide(currentSlideIdx + 1);
-  }
-});
+// nextBtn.addEventListener("click", () => {
+//   if (currentSlideIdx < slideCount - 1) {
+//     moveSlide(currentSlideIdx + 1);
+//   }
+// });
 
-prevBtn.addEventListener("click", () => {
-  if (currentSlideIdx > 0) {
-    moveSlide(currentSlideIdx - 1);
-  }
-});
+// prevBtn.addEventListener("click", () => {
+//   if (currentSlideIdx > 0) {
+//     moveSlide(currentSlideIdx - 1);
+//   }
+// });
 
-pagerBtn.forEach((item, idx) => {
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    moveSlide(idx);
-  });
-});
+// pagerBtn.forEach((item, idx) => {
+//   item.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     moveSlide(idx);
+//   });
+// });
 
-//자동 슬라이드
-function autoSlide() {
-  timer = setInterval(() => {
-    let nextIdx = (currentSlideIdx + 1) % slideCount;
-    moveSlide(nextIdx);
-  }, 3000);
-}
+// //자동 슬라이드
+// function autoSlide() {
+//   timer = setInterval(() => {
+//     let nextIdx = (currentSlideIdx + 1) % slideCount;
+//     moveSlide(nextIdx);
+//   }, 3000);
+// }
 
-autoSlide();
+// autoSlide();
 
-slideWrapper.addEventListener("mouseover", () => {
-  clearInterval(timer);
-});
+// slideWrapper.addEventListener("mouseover", () => {
+//   clearInterval(timer);
+// });
 
-slideWrapper.addEventListener("mouseout", () => {
-  autoSlide();
-});
+// slideWrapper.addEventListener("mouseout", () => {
+//   autoSlide();
+// });
 
 /* 이현정 구현 부분 종료*/ 
 
 /* 이강산 구현 부분 */ 
+
+// 변수 지정
+let slideWrapper = document.querySelector('.rc-slide-wrapper'), //ul의 부모
+    slideContainer = slideWrapper.querySelector('.rc-slide-container'), //ul
+    slides = slideContainer.querySelectorAll('li'),
+    slidesCount = slides.length,
+    CurrentSlideIdx = 0,
+    prevBtn = document.querySelector('#rc-prev'),
+    nextBtn = document.querySelector('#rc-next');
+
+
+//  슬라이드가 일단 있어야겠지? 근데 슬라이드가 하나 이상이니까 조건을 짜서 가로로 배열하는거겠지
+if(slidesCount > 1){
+  slides.forEach((item,idx)=>{
+    item.style.left = `${idx*100}%`
+  })
+}
+
+// 슬라이드 이동 함수
+function moveSlide(num){
+  slideContainer.style.left = `${num*-100}%`;
+  CurrentSlideIdx = num;
+
+  //마지막이면 disabled 클래스명 추가 아니면 제거
+  if(slidesCount - 1 == CurrentSlideIdx){
+    nextBtn.classList.add('disabled');
+  } else {
+    nextBtn.classList.remove('disabled');
+  }
+
+  //처음이면
+  if(CurrentSlideIdx === 0){
+    prevBtn.classList.add('disabled');
+  } else {
+    prevtBtn.classList.remove('disabled');
+  }
+}
+moveSlide(0);
+
+
+// 좌우버튼으로 슬라이드 넘기기
+  nextBtn.addEventListener('click',()=>{
+    if(slidesCount - 1 > CurrentSlideIdx){
+    moveSlide(CurrentSlideIdx + 1);
+    }
+  });
+//이전버튼
+  prevBtn.addEventListener('click',()=>{
+    if(CurrentSlideIdx > 0){
+    moveSlide(CurrentSlideIdx - 1);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // nextBtn.addEventLister('click',()=>{
+  //   //마지막이 아니라면
+  //   if(slideCount>)
+  //   moveSlide(CurrentSlideIdx + 1);
+  // });
+
+
+
+  
+
+
+
+
+
+
+
+
+
 
 /* 이강산 구현 부분 종료*/ 
 
